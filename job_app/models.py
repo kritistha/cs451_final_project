a5e2b68c1a37cd510555a31a02a4d3e99288bfda
kritistha <42780360+kritistha@users.noreply.github.com>
2018-12-17 12:05:06 -0700

    Update models.py

--- job_app/models.py
+++ job_app/models.py
@@ -3,3 +3,12 @@ from django.db import models
 from django.conf import settings 
 
 APP_LOCATION = settings.DEFAULT_LOCATION
+class Job(models.Model):
+	title = models.CharField(max_length=128)
+	salary = models.IntegerField()
+	address = models.CharField(max_length=128)
+	lat = models.FloatField()
+	lon = models.FloatField()
+	
+	def __str__(self):
+		return self.title
