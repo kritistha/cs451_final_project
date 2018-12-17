adc90838f3c354758b3d552abc11d37e568a3dc6
kritistha <42780360+kritistha@users.noreply.github.com>
2018-12-17 12:05:43 -0700

    Update views.py

--- job_app/views.py
+++ job_app/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
+from geopy import Nominatim
+from geopy import distance
+
+from django.views import generic 
 from django.conf import settings 
+from .models import Job
+
 
 APP_LOCATION = settings.DEFAULT_LOCATION
+
+class HomeView(generic.TemplateView):
+	template_name = 'home.html'
+
+
+class JobView(generic.DetailView):
+	model = Job
+	template_name = 'job.html'
+	
+
+class JobListView(generic.ListView):
+	model = Job
+	template_name = 'job_list.html'
+	
+class JobSearchView(generic.ListView):
+	model = Job
+	template_name = 'job_search.html'
+	
+	def get_context_data(self, **kwargs):
+		context = super(JobSearchView, self).get_context_data(**kwargs)
+        context['jobs'] = Job.objects.all()
+        return context
